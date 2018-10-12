b28c7fc283ba85e46a5afb7e4db48ab7448bd0c6
Ed Smith-Rowland <3dw4rd@verizon.net>
2018-10-11 20:33:26 -0400

    Start testing solution_t.

---
+++ test_solution.cpp
@@ -0,0 +1,14 @@
+/*
+$HOME/bin/bin/g++ -std=gnu++17 -g -Wall -Wextra -Wno-psabi -I. -I.. -o test_solution test_solution.cpp -lquadmath
+LD_LIBRARY_PATH=$HOME/bin/lib64:$LD_LIBRARY_PATH ./test_solution > test_solution.txt
+*/
+
+#include <ext/solution.h>
+
+int
+main()
+{
+  __gnu_cxx::solution_t<double> s1(3.0);
+  auto s2 = __gnu_cxx::solution_t<double>(std::complex<double>{1.0, 3.0});
+  auto x1 = 4.0 * s1;
+}
