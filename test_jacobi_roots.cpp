99232c86bcf5eb3be1c4fa4405e387c1526ae170
emsr <3dw4rd@verizon.net>
2019-02-05 15:37:46 -0500

    Try to get Jacobi roots correct.

--- test_jacobi_roots.cpp
+++ test_jacobi_roots.cpp
@@ -10,37 +10,109 @@ LD_LIBRARY_PATH=$HOME/bin/lib64:$LD_LIBRARY_PATH ./test_jacobi_roots > test_jaco
 #include <iomanip>
 
 #include <ext/solver_jenkins_traub.h>
-#include <ext/math_const.h>
-#include <bits/specfun.h>
+#include <ext/polynomial.h>
+
+// Just grab this Jacobi polynomial from tr29124_test.
+namespace std
+{
+namespace __detail
+{
+  /**
+   * Return the Jacobi polynomial as a polynomial.
+   *
+   * @tparam _Tp The real type of the argument and degree parameters.
+   * @param[in]  n  The order of the Jacobi polynomial
+   * @param[in]  alpha1  The first parameter of the Jacobi polynomial
+   * @param[in]  beta1  The second parameter of the Jacobi polynomial
+   */
+  template<typename _Tp>
+    __gnu_cxx::_Polynomial<_Tp>
+    __jacobi_poly(unsigned int __n, _Tp __alpha1, _Tp __beta1)
+    {
+      __gnu_cxx::_Polynomial<_Tp> __poly;
+      const __gnu_cxx::_Polynomial<_Tp> __arg{_Tp{0.5L}, _Tp{-0.5L}};
+
+      if (std::isnan(__alpha1) || std::isnan(__beta1))
+	return __poly;
+
+      auto __term = __gnu_cxx::_Polynomial<_Tp>{1};
+      __poly += __term;
+      if (__n == 0)
+	return __poly;
+
+      auto __fact = _Tp{1};
+      const auto __ab = __alpha1 + __beta1;
+      for (unsigned int __k = 1; __k <= __n; ++__k)
+	{
+	  __fact *= _Tp(__alpha1 + __k) / _Tp(__k);
+	  __term *= _Tp(-__n + __k - 1) / _Tp(__k)
+		  * _Tp(__n + __k + __ab) / _Tp(__alpha1 + __k)
+		  * __arg;
+	  __poly += __term;
+	}
+
+      return __fact * __poly;
+    }
+} // namespace std
+} // namespace __detail
 
 template<typename _Tp>
   void
-  test(unsigned n, _Tp alpha1, _Tp beta1, std::ofstream& gp)
+  test_jacobi_roots(unsigned n, _Tp alpha1, _Tp beta1, std::ofstream& gp)
   {
-    auto poly = std::__detail::__jacobi_poly(n, alpha1, beta1);
+    const auto prec = std::numeric_limits<_Tp>::digits10;
+    const auto w = 6 + prec;
+
+    std::cout << std::setprecision(prec);
+    std::cout << "\n\n";
+    std::cout << " n = " << n
+	      << "; alpha = " << alpha1
+	      << "; beta = " << beta1 << '\n';
+
+    const auto poly = std::__detail::__jacobi_poly(n, alpha1, beta1);
+    auto coef = poly.coefficients();
     std::cout << "\nThe polynomial coefficients are:\n";
-    for (const auto& c : poly)
-      std::cout << c << '\n';
+    for (const auto& c : coef)
+      std::cout << std::setw(w) << c << '\n';
 
-    std::reverse(poly.begin(), poly.end());
+    std::reverse(coef.begin(), coef.end());
 
-    auto jt = __gnu_cxx::_JenkinsTraubSolver(poly);
+    auto jt = __gnu_cxx::_JenkinsTraubSolver(coef);
     auto roots = jt.solve();
     std::cout << "\nThe roots are:\n";
-    for (const auto& z : roots)
-      std::cout << z << '\n';
-
-    gp << "\n\n";
     for (const auto& z : roots)
       {
 	if (z.index() == 0)
 	  continue;
 	else if (z.index() == 1)
-	  gp << "  " << std::get<1>(z)
-	     << "  " << 0.0 << '\n';
+	  std::cout << ' ' << std::setw(w) << std::get<1>(z)
+		    << ' ' << std::setw(w) << 0.0
+		    << ' ' << std::setw(w) << poly(std::get<1>(z))
+		    << '\n';
 	else
-	  gp << "  " << std::real(std::get<2>(z))
-	     << "  " << std::imag(std::get<2>(z)) << '\n';
+	  std::cout << ' ' << std::setw(w) << std::real(std::get<2>(z))
+		    << ' ' << std::setw(w) << std::imag(std::get<2>(z))
+		    << ' ' << std::setw(w) << poly(std::get<2>(z))
+		    << '\n';
+      }
+
+    gp << std::setprecision(prec);
+    gp << "\n\n";
+    gp << "# n = " << n
+       << "; alpha = " << alpha1
+       << "; beta = " << beta1 << '\n';
+    for (const auto& z : roots)
+      {
+	if (z.index() == 0)
+	  continue;
+	else if (z.index() == 1)
+	  gp << ' ' << std::setw(w) << std::get<1>(z)
+	     << ' ' << std::setw(w) << 0.0
+	     << '\n';
+	else
+	  gp << ' ' << std::setw(w) << std::real(std::get<2>(z))
+	     << ' ' << std::setw(w) << std::imag(std::get<2>(z))
+	     << '\n';
       }
     gp << std::flush;
   }
@@ -57,29 +129,31 @@ template<typename _Tp>
     unsigned n = 50;
     _Tp alpha1, beta1;
 
-    alpha1 = 2.0L;
-    beta1 = -42.5L;
-    test(n, alpha1, beta1, gp);
+    alpha1 = _Tp{2.0L};
+    beta1 = _Tp{-42.5L};
+    test_jacobi_roots(n, alpha1, beta1, gp);
 
-    alpha1 = 2.0L;
-    beta1 = -52.1L;
-    test(n, alpha1, beta1, gp);
+    alpha1 = _Tp{2.0L};
+    beta1 = _Tp{-52.1L};
+    test_jacobi_roots(n, alpha1, beta1, gp);
 
-    alpha1 = 2.0L;
-    beta1 = -63.5L;
-    test(n, alpha1, beta1, gp);
+    alpha1 = _Tp{2.0L};
+    beta1 = _Tp{-63.5L};
+    test_jacobi_roots(n, alpha1, beta1, gp);
 
-    alpha1 = 2.0L;
-    beta1 = -42.5L;
-    test(n, beta1, alpha1, gp);
+    // Flip alpha and beta.
 
-    alpha1 = 2.0L;
-    beta1 = -52.1L;
-    test(n, beta1, alpha1, gp);
+    alpha1 = _Tp{2.0L};
+    beta1 = _Tp{-42.5L};
+    test_jacobi_roots(n, beta1, alpha1, gp);
 
-    alpha1 = 2.0L;
-    beta1 = -63.5L;
-    test(n, beta1, alpha1, gp);
+    alpha1 = _Tp{2.0L};
+    beta1 = _Tp{-52.1L};
+    test_jacobi_roots(n, beta1, alpha1, gp);
+
+    alpha1 = _Tp{2.0L};
+    beta1 = _Tp{-63.5L};
+    test_jacobi_roots(n, beta1, alpha1, gp);
   }
 
 int
