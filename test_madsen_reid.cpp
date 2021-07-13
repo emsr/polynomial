badcae2d2490d2e610a4f642d34e45cc77392fdd
Ed Smith-Rowland <3dw4rd@verizon.net>
2021-07-13 17:03:16 -0400

    Fair amout of reorg and sane control. Only two goto's left.

--- test_madsen_reid.cpp
+++ test_madsen_reid.cpp
@@ -1,18 +1,19 @@
 /*
  g++ -g -o test_madsen_reid test_madsen_reid.cpp
- ./test_madsen_reid < test/input/complex_solver1.in
- ./test_madsen_reid < test/input/complex_solver2.in
+ ./test_madsen_reid < test/input/complex_solver1.in > out2
+ ./test_madsen_reid < test/input/complex_solver2.in >> out2
 
- ./test_madsen_reid < test/input/test_solver1.in
- ./test_madsen_reid < test/input/test_solver2.in
- ./test_madsen_reid < test/input/test_solver3.in
- ./test_madsen_reid < test/input/test_solver4.in
- ./test_madsen_reid < test/input/test_solver5.in
- ./test_madsen_reid < test/input/test_solver6.in
- ./test_madsen_reid < test/input/test_solver7.in
- ./test_madsen_reid < test/input/test_solver8.in
+ ./test_madsen_reid < test/input/test_solver1.in >> out2
+ ./test_madsen_reid < test/input/test_solver2.in >> out2
+ ./test_madsen_reid < test/input/test_solver3.in >> out2
+ ./test_madsen_reid < test/input/test_solver4.in >> out2
+ ./test_madsen_reid < test/input/test_solver5.in >> out2
+ ./test_madsen_reid < test/input/test_solver6.in >> out2
+ ./test_madsen_reid < test/input/test_solver7.in >> out2
+ ./test_madsen_reid < test/input/test_solver8.in >> out2
 */
 
+#include <limits>
 #include <complex>
 #include <vector>
 #include <iostream>
@@ -24,7 +25,7 @@ using Cmplx = std::complex<Real>;
  * Return the L1 sum of absolute values or Manhattan metric of a complex number.
  */
 Real
-norml1(Cmplx z)
+norm_l1(Cmplx z)
 {
     return std::abs(std::real(z)) + std::abs(std::imag(z));
 }
@@ -33,7 +34,7 @@ norml1(Cmplx z)
  * Return the L2 modulus of the complex number (this is std::norm).
  */
 Real
-norml2(Cmplx z)
+norm_l2(Cmplx z)
 {
     return std::norm(z);
 }
@@ -53,7 +54,30 @@ eval(Cmplx z, Cmplx& fz, int n1, const std::vector<Cmplx>& a)
 
     fz = p;
 
-    return norml2(p);
+    return norm_l2(p);
+}
+
+/**
+ * Store the root.
+ */
+void
+push_root(std::vector<Cmplx>& a1, std::vector<Cmplx>& root, int& n, Cmplx z)
+{
+    a1[n] = root[n];
+    root[n] = z;
+    --n;
+}
+
+/**
+ * Store the root.
+ */
+void
+deflate(std::vector<Cmplx>& a, int n, Cmplx z)
+{
+    for (int k = 2; k <= n; ++k)
+    {
+        a[k] = a[k - 1] * z + a[k];
+    }
 }
 
 //
@@ -62,8 +86,6 @@ eval(Cmplx z, Cmplx& fz, int n1, const std::vector<Cmplx>& a)
 void
 solve(std::vector<Cmplx>& a1, int m, std::vector<Cmplx>& root, std::vector<Cmplx>& a, int mp1)
 {
-
-    //Cmplx a1(m+1), root(m), a(mp1)
     Cmplx z0, f0z, z, dz, f1z, fz, w, fw, dzk;
     Real f0, ff, f, fa, fmin, f2;
     bool stage1, div2;
@@ -72,11 +94,11 @@ solve(std::vector<Cmplx>& a1, int m, std::vector<Cmplx>& root, std::vector<Cmplx
 
     int i, n1;
 
-    const Real BIG = 1.0e+70; // Overflow limit
-    const Real SMALL = 1.0e-70; // Underflow limit.
-    //data BASE/16;
-    const Real BASE = 2;
-    const Real EPS = 2.3e-16;
+    const Real DIGITS = std::numeric_limits<Real>::max_digits10;
+    const Real BIG = std::numeric_limits<Real>::max(); // Overflow limit
+    const Real SMALL = std::numeric_limits<Real>::min(); // Underflow limit.
+    const Real BASE = std::numeric_limits<Real>::radix; // 16 -> 2
+    const Real EPS = std::numeric_limits<Real>::epsilon();
 
     const Real SSMALL = std::sqrt(SMALL);
     const Real ALOGB = std::log(BASE);
@@ -94,7 +116,7 @@ solve(std::vector<Cmplx>& a1, int m, std::vector<Cmplx>& root, std::vector<Cmplx
     }
 
     // Test for zeros at infinity.
-    while (norml1(a[1]) <= 0 && n > 0)
+    while (norm_l1(a[1]) <= 0 && n > 0)
     {
         for (int i = 1; i <= n; ++i)
         {
@@ -103,213 +125,201 @@ solve(std::vector<Cmplx>& a1, int m, std::vector<Cmplx>& root, std::vector<Cmplx
         root[n] = BIG;
         --n;
     }
-    if (n <= 0)
-        return;
 
- _40:
-    if (n <= 1) {
-        goto _260;
-    }
-
-    n1 = n + 1;
-
-    // Scale the coefficients.
-    u1 = 0.0;
-    u2 = BIG;
-    for(int k = 1; k <= n1; ++k)
+    while (n >= 1)
     {
-        u = norml1(a[k]);
-        if (u <= 0.0) {
+        if (n == 1)
+        {
+            z = -a[2] / a[1];
+            a1[n] = root[n];
+            root[n] = z;
+            return;
+        }
+
+        n1 = n + 1;
+
+        // Scale the coefficients.
+        u1 = 0.0;
+        u2 = BIG;
+        for(int k = 1; k <= n1; ++k)
+        {
+            u = norm_l1(a[k]);
+            if (u <= 0.0) {
+                continue;
+            }
+            if (u > u1) {
+                u1 = u;
+            }
+            if (u < u2) {
+                u2 = u;
+            }
+        }
+        u = std::sqrt(u1) * std::sqrt(u2);
+        i = -std::log(u) / ALOGB; // ilogb?
+        u = std::pow(BASE, Real(i));
+        for (int k = 1; k <= n; ++k)
+        {
+            a[k] = u * a[k];
+            a1[k] = a[k] * Real(n1 - k);
+        }
+        a[n1] = u * a[n1];
+
+        // Test for zeros at (0, 0)
+        z = Cmplx(0, 0);
+        if (norm_l1(a[n1]) <= SSMALL)
+        {
+            push_root(a1, root, n, z);
             continue;
         }
-        if (u > u1) {
-            u1 = u;
-        }
-        if (u < u2) {
-            u2 = u;
-        }
-    }
-    u = std::sqrt(u1) * std::sqrt(u2);
-    i = -std::log(u) / ALOGB; // ilogb?
-    u = std::pow(BASE, Real(i));
-    for (int k = 1; k <= n; ++k)
-    {
-        a[k] = u * a[k];
-        a1[k] = a[k] * Real(n1 - k);
-    }
-    a[n1] = u * a[n1];
+        z0 = Cmplx(0, 0);
+        f0 = norm_l2(a[n1]);
+        fmin = f0 * std::pow(Real(n) * DIGITS * EPS, 2);
 
-    // Test for zeros at (0, 0)
-    z = Cmplx(0, 0);
-    if (norml1(a[n1]) <= SSMALL)
-    {
-        goto _290;
-    }
-    z0 = Cmplx(0, 0);
-    f0 = norml2(a[n1]);
-    fmin = f0 * std::pow(Real(n) * 2 * EPS, 2); // 16 -> 2
+        // z is the current point
+        // f = |f(z)|^2
+        // z0 is the last point
+        // f0z = f'(z0)
+        // f0 = |f(z0)|^2
+        // r0 = 3|z - z0|
+        // dz is the last tentative step if the last step was successful or is the required next step.
+        ff = f0;
+        u0 = f0;
+        t = BIG;
+        for (int k = 1; k <= n; ++k)
+        {
+            u = norm_l2(a[k]);
+            if (u == Real{0}) {
+                continue;
+            }
+            u = log(u0 / u) / Real(2 *(n1 - k));
+            if (u < t) {
+                t = u;
+            }
+        }
+        t = std::exp(t);
+        f0z = a[n];
+        z = Cmplx(1, 0);
+        if (norm_l1(f0z) > Real{0})
+        {
+            z = -a[n1] / a[n];
+        }
+        u = 0.5 * t / norm_l1(z);
+        z = u * z;
+        dz = z;
+        f =  eval(z, fz, n + 1, a);
+        r0 = 0.5 * t;
 
-    // z is te current point
-    // f = |f(z)|^2
-    // z0 is the last point
-    // f0z = f'(z0)
-    // f0 = |f(z0)|^2
-    // r0 = 3|z - z0|
-    // dz is the last tentative step if the last step was successful or is the required next step.
-    ff = f0;
-    u0 = f0;
-    t = BIG;
-    for (int k = 1; k <= n; ++k)
-    {
-        u = norml2(a[k]);
-        if (u == 0.0) {
+        // Calculate tentative step.
+    _120:
+        u = eval(z, f1z, n, a1);
+        if (u == 0.0)
+        {
+            dz = Cmplx(1.8 * std::real(dz) - 2.4 * std::imag(dz),
+                       2.4 * std::real(dz) + 1.8 * std::imag(dz));
+            stage1 = true;
+        }
+        else
+        {
+            dz = -fz / f1z;
+            f2 = norm_l2(f0z - f1z) / norm_l2(z0 - z);
+            stage1 = (f * f2 / u > 0.25 * u) || (f != ff);
+            r = norm_l1(dz);
+            if (r > 3.0 * r0) {
+                dz = Cmplx(1.8 * std::real(dz) - 2.4 * std::imag(dz) * r0 / r,
+                           2.4 * std::real(dz) + 1.8 * std::imag(dz) * r0 / r);
+            }
+        }
+
+        f0z = f1z;
+
+        // Find the next point in the iteration.
+        // This is where iteration starts if the previous one was unsuccessful.
+    _160:
+        z0 = z;
+        f0 = f;
+        dzk = dz;
+        z = z0 + dz;
+        // If either part of z is small replace by zero to avoid underflows.
+        if (std::abs(std::real(z)) < EPS * std::abs(std::imag(z))) {
+            z = Cmplx(0, std::imag(z));
+        }
+        if (std::abs(std::imag(z)) < EPS * std::abs(std::real(z))) {
+            z = Cmplx(std::real(z), 0.0);
+        }
+        w = z;
+        f = eval(z, fz, n + 1, a);
+        ff = f;
+        if (stage1)
+        {
+            j = 1;
+            div2 = f >= f0;
+
+            do
+            {
+                if (div2)
+                {
+                    dz = 0.5 * dz;
+                    w = z0 + dz;
+                }
+                else
+                {
+                    w += dz;
+                }
+
+                fa = eval(w, fw, n + 1, a);
+                if (fa >= f)
+                {
+                    break;
+                }
+                f = fa;
+                fz = fw;
+                z = w;
+                ++j;
+                if (div2 && j == 3)
+                {
+                    dz = Cmplx(0.6 * std::real(dz) - 0.8 * std::imag(dz),
+                               0.8 * std::real(dz) + 0.6 * std::imag(dz));
+                    z = z0 + dz;
+                    f = eval(z, fz, n + 1, a);
+                    break;
+                }
+            }
+            while (j <= n);
+        }
+
+        r0 = norm_l1(z0 - z);
+
+        // Convergence test.
+        if (f >= f0)
+        {
+            z = z0;
+        }
+
+        r1 = norm_l1(z);
+        if (r0 < EPS * r1)
+        {
+            deflate(a, n, z);
+            push_root(a1, root, n, z);
             continue;
         }
-        u = log(u0 / u) / Real(2 *(n1 - k));
-        if (u < t) {
-            t = u;
+
+        if (f < f0)
+        {
+            goto _120;
         }
-    }
-    t = std::exp(t);
-    f0z = a[n];
-    z = Cmplx(1, 0);
-    if (norml1(f0z) > Real{0})
-    {
-        z = -a[n1] / a[n];
-    }
-    u = 0.5 * t / norml1(z);
-    z = u * z;
-    dz = z;
-    f =  eval(z, fz, n + 1, a);
-    r0 = 0.5 * t;
 
-    // Calculate tentative step.
-_120:
-    u = eval(z, f1z, n, a1);
-    if (u == 0.0) {
-        goto _140;
-    }
-    dz = -fz / f1z;
-    f2 = norml2(f0z - f1z) / norml2(z0 - z);
-    stage1 = (f * f2 / u > 0.25 * u) || (f != ff);
-    r = norml1(dz);
-    if (r <= 3.0 * r0) {
-        goto _150;
-    }
+        f = f0;
+        if (f < fmin)
+        {
+            deflate(a, n, z);
+            push_root(a1, root, n, z);
+            continue;
+        }
 
-    dz = Cmplx(1.8 * std::real(dz) - 2.4 * std::imag(dz) * r0 / r,
-               2.4 * std::real(dz) + 1.8 * std::imag(dz) * r0 / r);
-    goto _150;
-
-_140:
-    dz = Cmplx(1.8 * std::real(dz) - 2.4 * std::imag(dz),
-               2.4 * std::real(dz) + 1.8 * std::imag(dz));
-    stage1 = true;
-
-_150:
-    f0z = f1z;
-
-    // Find the next point in te iteration.
-    // This is where iteration starts if the previous one was unsuccessful.
-_160:
-    z0 = z;
-    f0 = f;
-    dzk = dz;
-    z = z0 + dz;
-    // If either part of z is small replace by zero to avoid underflows.
-    if (std::abs(std::real(z)) < EPS * std::abs(std::imag(z))) {
-        z = Cmplx(0, std::imag(z));
-    }
-    if (std::abs(std::imag(z)) < EPS * std::abs(std::real(z))) {
-        z = Cmplx(std::real(z), 0.0);
-    }
-    w = z;
-    f = eval(z, fz, n + 1, a);
-    ff = f;
-    if (!stage1) {
-        goto _240;
-    }
-
-    // Beginning of stage 1 search.
-    j = 1;
-    div2 = f >= f0;
-_180:
-    if (div2) {
-        dz = 0.5 * dz;
-        w = z0 + dz;
-    }else{
-        w += dz;
-    }
-
-    fa = eval(w, fw, n + 1, a);
-    if (fa >= f) {
-        goto _240;
-    }
-    f = fa;
-    fz = fw;
-    z = w;
-    ++j;
-    if (div2 && j == 3) {
-        goto _220;
-    }
-    if (j <= n) {
-        goto _180;
-    }
-    goto _240;
-_220:
-    dz = Cmplx(0.6 * std::real(dz) - 0.8 * std::imag(dz),
-               0.8 * std::real(dz) + 0.6 * std::imag(dz));
-    z = z0 + dz;
-    f = eval(z, fz, n + 1, a);
-
-    // End of stage 1 search.
-
-_240:
-    r0 = norml1(z0 - z);
-
-    // Convergence test.
-    if (f < f0) {
-        goto _250;
-    }
-    z = z0;
-_250:
-    r1 = norml1(z);
-    if (r0 < EPS * r1) {
-        goto _270;
-    }
-    if (f < f0) {
-        goto _120;
-    }
-    f = f0;
-    if (f < fmin) {
-        goto _270;
-    }
-    dz = Cmplx(-0.3 * std::real(dzk) + 0.4 * std::imag(dzk),
-               -0.4 * std::real(dzk) - 0.3 * std::imag(dzk));
-    stage1 = true;
-    goto _160;
-
-_260:
-    // Deal with n == 1 case.
-    z = -a[2] / a[1];
-    goto _290;
-
-    // Deflate, store root, restore coefficient of original polynomial.
-_270:
-    for (int k = 2; k <= n; ++k)
-    {
-        a[k] = a[k - 1] * z + a[k];
-    }
-_290:
-    a1[n] = root[n];
-    root[n] = z;
-    --n;
-    if (n < 1) {
-        return;
-    }else if (n == 1) {
-        goto _260;
-    }else{
-        goto _40;
+        dz = Cmplx(-0.3 * std::real(dzk) + 0.4 * std::imag(dzk),
+                   -0.4 * std::real(dzk) - 0.3 * std::imag(dzk));
+        stage1 = true;
+        goto _160;
     }
 
     return;
